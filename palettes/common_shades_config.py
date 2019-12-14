82b357a9c4e0152f7d12ce6bbfb983d591193ad7
Jan Warchoł <jan.warchol@gmail.com>
2019-12-14 11:52:31 +0100

    VSCode-0.2: Increase fg-dim contrast
    Having an intermediate shade between main foreground and comment color
    was too confusing. Fortunately bold comment color is quite more visible
    than normal comment, which allows using it for tokens like types. This
    way first plane (code) and second plane (comments) are clearly separate.
    
    Also, make entity names more visible.

--- palettes/common_shades_config.py
+++ palettes/common_shades_config.py
@@ -3,8 +3,8 @@ from __future__ import division
 shades_lightness = {
     "bg_1":  1/10,
     "bg_2":  1/4,
-    "dim_0": 3/5,
-    "dim_1": 4/5,
+    "dim_0": 2/4,
+    "dim_1": 3/4,
     "fg_1":  1 + 1/8,
-    "fg_2":  1 + 1/3,
+    "fg_2":  1 + 1/4,
 }
