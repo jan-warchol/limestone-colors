daf6d7ecd9396a877583b6a31596bcc2cd3ddea2
Jan Warchoł <jan.warchol@gmail.com>
2019-12-14 10:20:41 +0100

    Add separate palette spec based on vscode theme

---
+++ palettes/common_shades_config.py
@@ -0,0 +1,10 @@
+from __future__ import division
+
+shades_lightness = {
+    "bg_1":  1/10,
+    "bg_2":  1/4,
+    "dim_0": 3/5,
+    "dim_1": 4/5,
+    "fg_1":  1 + 1/8,
+    "fg_2":  1 + 1/3,
+}
---
+++ palettes/vscode_mono.py
@@ -0,0 +1,7 @@
+import common_shades_config
+
+# Variant based on fg and bg lightness of default VScode dark theme
+name = "VScode-based mono"
+background = (11, 0, 0)
+foreground = (78, 0, 0)
+shades = common_shades_config.shades_lightness
