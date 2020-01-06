21e6b19d5efed26f792f6eda301bd775989020c0
Jan Warchoł <jan.warchol@gmail.com>
2020-01-07 00:26:23 +0100

    Add palette designed for people with deuteranopia

---
+++ palettes/deuteranopia.py
@@ -0,0 +1,13 @@
+import common_color_config
+
+# uses accent colors that should be best visible for people with deuteranopia
+name = "deuteranopia"
+background = (15, 0, 0)
+foreground = (75, 0, 0)
+shades = common_color_config.shades_lightness
+
+colors = {
+    "primary":   (75, -5,  75),
+    "secondary": (60, 31, -62),
+}
+color_variants = common_color_config.accent_variants
