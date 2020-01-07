9a8b3cb54e2c8c3dace01c073d520240b113e9d7
Jan Warchoł <jan.warchol@gmail.com>
2020-01-07 23:10:11 +0100

    WIP selenized palette

---
+++ palettes/selenized.py
@@ -0,0 +1,7 @@
+import common_color_config
+
+# based on Solarized, but lighter and with more contrast
+name = "Selenized mono 60"
+background = (20, 12, 12)
+foreground = (75, -5, -2)
+shades = common_color_config.shades_lightness
