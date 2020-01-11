fc85dd3cd0a133ee11c5caab6470ab1f3ac1a91c
Jan Warchoł <jan.warchol@gmail.com>
2020-01-11 15:41:53 +0100

    Add palette and styling rules using two accent colors

---
+++ token_styling_dual.py
@@ -0,0 +1,48 @@
+"""Define styling (color and formatting) for common token types.
+
+Color should be a name available in Palette.color_coords dictionary.
+Formatting should be either "normal" or a combination of
+- boid
+- italic
+- underline
+(order doesn't matter)."""
+
+rules = {
+    # non-code (information and metadata)
+    "comment":         ("dim_0", "italic"),
+    "doc_comment":     ("dim_0", "normal"),
+    "doc_special":     ("dim_1", "bold"),
+
+    # literals and constants
+    "string":          ("dim_2", "normal"),
+    "str_special":     ("fg_2",  "normal"),
+    "number":          ("secondary",  "normal"),
+    "boolean":         ("secondary",  "italic"),
+    "constant":        ("fg_0",  "italic"),
+
+    # language reserved words
+    "keyword":         ("fg_1",  "bold"),
+    "import":          ("primary",  "bold"),
+    "operator":        ("primary",  "normal"),
+
+    # "meta" tokens that supplement other elements
+    "type":            ("dim_1", "bold italic"),
+    "keyword_arg":     ("dim_3", "italic"),
+    "special_var":     ("dim_1", "bold italic"),
+
+    # elements that are usually predefined
+    "built_in":        ("primary_2_3",  "normal"),
+    "exception":       ("fg_2",  "bold"),
+    "decorator":       ("primary_2_3",  "italic"),
+
+    # elements that are usually user-defined
+    "class_def":       ("fg_1",  "bold underline"),
+    "function_def":    ("fg_1",  "underline"),
+    "function_call":   ("fg_0",  "normal"),
+    # some grammars use one scope for function definitions and calls
+    "function":        ("fg_0",  "normal"),
+    "parameter":       ("fg_0",  "normal"),
+
+    # other
+    "punctuation":     ("fg_2",  "normal"),
+}
