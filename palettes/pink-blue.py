d7b6160a5427c16486e25b99821a1006bbe30871
Jan Warchoł <jan.warchol@gmail.com>
2020-01-31 16:37:25 +0100

    Add two pink variants for Ece

---
+++ palettes/pink-blue.py
@@ -0,0 +1,13 @@
+import common_color_config
+
+# average JetBrains, VScode and Sublime bg lightness; moderate contrast
+name = "Pink for Ece v1"
+background = (15, 0, 0)
+foreground = (75, 0, 0)
+shades = common_color_config.shades_lightness
+
+colors = {
+    "primary":   (75,   6,  66),
+    "secondary": (70,  55, -15),
+}
+color_variants = common_color_config.accent_variants
---
+++ palettes/yellow-pink.py
@@ -0,0 +1,13 @@
+import common_color_config
+
+# average JetBrains, VScode and Sublime bg lightness; moderate contrast
+name = "Pink for Ece v2"
+background = (15, 0, 0)
+foreground = (75, 0, 0)
+shades = common_color_config.shades_lightness
+
+colors = {
+    "primary":   (70,  55, -15),
+    "secondary": (65, -13, -54),
+}
+color_variants = common_color_config.accent_variants
