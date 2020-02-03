7c90f12b1b27c0a6ca6dd99b3f3a6754e3aeaf76
Jan Warchoł <jan.warchol@gmail.com>
2020-02-03 23:51:31 +0100

    WIP try sepia with blue and green

---
+++ palettes/limestone_light.py
@@ -0,0 +1,13 @@
+import common_color_config
+
+# average JetBrains, VScode and Sublime bg lightness; moderate contrast
+name = "Limestone light"
+background = (97, 0, 12)
+foreground = (37, 0, 0)
+shades = common_color_config.shades_lightness
+
+colors = {
+    "primary":   (42, -42, 45),
+    "secondary": (40, 19, -73),
+}
+color_variants = common_color_config.accent_variants
