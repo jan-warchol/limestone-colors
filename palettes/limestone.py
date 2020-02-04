bb0078463a7a1f5dab428672f6780dc3ab9b5363
Jan Warchoł <jan.warchol@gmail.com>
2020-02-04 08:38:25 +0100

    initial attempt, quite ugly

--- palettes/limestone.py
+++ palettes/limestone.py
@@ -2,12 +2,12 @@ import common_color_config
 
 # average JetBrains, VScode and Sublime bg lightness; moderate contrast
 name = "Limestone"
-background = (15, 0, 0)
-foreground = (75, 0, 0)
+background = (30, 0, 0)
+foreground = (90, 0, 0)
 shades = common_color_config.shades_lightness
 
 colors = {
-    "primary":   (75,   6,  66),
-    "secondary": (65, -13, -54),
+    "primary":   (90,   6,  66),
+    "secondary": (80, -13, -54),
 }
 color_variants = common_color_config.accent_variants
