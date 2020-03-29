1b340d51559046c14aa36b86644a0cb5c25c911a
Jan Warchoł <jan.warchol@gmail.com>
2020-03-29 21:31:45 +0200

    Add selenized and selenized light palettes

---
+++ palettes/selenized.py
@@ -0,0 +1,19 @@
+import common_color_config
+
+# My fork of venerable solarized
+name = "Selenized"
+background = (23, -12, -12)
+foreground = (75, -5, -2)
+shades = common_color_config.shades_lightness
+
+colors = {
+    "red":     (60,  63,  40),
+    "green":   (69, -38,  55),
+    "yellow":  (75,   6,  68),
+    "blue":    (60,   0, -57),
+    "magenta": (66,  55, -15),
+    "cyan":    (73, -40,  -4),
+    "orange":  (67,  37,  50),
+    "violet":  (64,  30, -45),
+}
+color_variants = common_color_config.accent_variants
---
+++ palettes/selenized_light.py
@@ -0,0 +1,19 @@
+import common_color_config
+
+# My fork of venerable solarized
+name = "Selenized light"
+background = (96, 0, 13)
+foreground = (42, -6, -6)
+shades = common_color_config.shades_lightness
+
+colors = {
+    "red":     (46,  66,  42),
+    "green":   (54, -40,  58),
+    "yellow":  (59,   6,  71),
+    "blue":    (46,   0, -60),
+    "magenta": (52,  58, -16),
+    "cyan":    (57, -42,  -4),
+    "orange":  (52,  39,  52),
+    "violet":  (49,  32, -47),
+}
+color_variants = common_color_config.accent_variants
